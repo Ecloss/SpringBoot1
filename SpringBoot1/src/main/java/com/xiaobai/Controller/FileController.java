0244c2355c8958996ad9cac3fdc743fc9e97a4e5
Ecloss <18797818311@163.com>
2019-05-07 11:48:12 +0800

    文件上传

---
+++ SpringBoot1/src/main/java/com/xiaobai/Controller/FileController.java
@@ -0,0 +1,69 @@
+package com.xiaobai.Controller;
+
+import com.xiaobai.model.JsonData;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+
+/**
+ * 文件测试
+ *
+ * @author 余修文
+ * @date 2019/5/7 10:14
+ */
+@Controller
+public class FileController {
+
+    @RequestMapping(value = "/api/v1/gopage")
+    public Object index() {
+        return "index";
+    }
+
+    public static final String filePath = "E:\\003_Code\\SpringBoot\\SpringBoot1\\src\\main\\resource\\static\\images";
+
+    @RequestMapping(value = "upload")
+    @ResponseBody
+    public JsonData upload(@RequestParam("head_img") MultipartFile file,
+                           HttpServletRequest request) {
+        if (file.isEmpty()) {
+            return new JsonData(-1, "文件不能为空！", null);
+        }
+        if (file.getSize() > 1024 * 1024) {
+            return new JsonData(-1, "文件太大！", null);
+        }
+        String name  = request.getParameter("name");
+        System.out.println("用户名：" + name);
+
+        String fileName = file.getOriginalFilename();
+        System.out.println("上传的文件名为：" + fileName);
+
+        String suffixName = fileName.substring(fileName.lastIndexOf("."));
+        System.out.println("上传的后缀名为：" + suffixName);
+
+        // 文件上传后的路径
+        fileName = UUID.randomUUID() + suffixName;
+        System.out.println("转换后的名称:"+fileName);
+
+        File dest = new File(filePath + fileName);
+
+        try {
+            file.transferTo(dest);
+
+            return new JsonData(0, fileName);
+        } catch (IllegalStateException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return new JsonData(-1, "fail to save", null);
+    }
+
+}
+
--- SpringBoot1/src/main/java/com/xiaobai/XdclassApplication.java
+++ SpringBoot1/src/main/java/com/xiaobai/XdclassApplication.java
@@ -2,6 +2,11 @@ package com.xiaobai;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.MultipartConfigElement;
 
 /**
  * 所有启动类的入口
@@ -14,6 +19,16 @@ public class XdclassApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(XdclassApplication.class,args);
+    }
+
+    @Bean
+    public MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        // 单个文件最大 10M
+        factory.setMaxFileSize("10240KB");
+        // 设置总上传数据总大小 1GB
+        factory.setMaxRequestSize("1024000");
+        return factory.createMultipartConfig();
     }
 
 }
