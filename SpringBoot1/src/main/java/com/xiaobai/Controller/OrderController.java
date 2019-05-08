e65e3caebe872598b1693d200b404f652a92026f
Ecloss <18797818311@163.com>
2019-05-08 22:13:55 +0800

    SpringBoot 单元测试

---
+++ SpringBoot1/src/main/java/com/xiaobai/Controller/OrderController.java
@@ -0,0 +1,22 @@
+package com.xiaobai.Controller;
+
+import com.xiaobai.model.JsonData;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 订单测试
+ *
+ * @author 余修文
+ * @date 2019/5/7 22:43
+ */
+@RestController
+@RequestMapping("/api/v1")
+public class OrderController {
+
+    //@GetMapping(value = "/order")
+    //public Object order(String msg) {
+    //}
+
+}
